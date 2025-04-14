627240ae155b5feb637538ee5279c4529fddffee
silver_volt4 <gdpancake5@gmail.com>
2025-04-14 11:37:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {target: "es2020"}
+  build: {target: "esnext"}
 })
