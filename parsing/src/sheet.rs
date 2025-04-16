49f433f89a95daab4f7e0fecb5b648b64ed83374
CenTdemeern1 <timo.herngreen@gmail.com>
2025-04-16 12:27:23 +0200

    Cleanup

--- parsing/src/sheet.rs
+++ parsing/src/sheet.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fmt::Display, io::Cursor};
+use std::io::{Cursor, Error as IoError};
 
 use crate::{bits::PickBit, read::ReadExt};
 
@@ -16,26 +16,8 @@ pub struct SheetStroke {
     pub style_bold: bool,
 }
 
-#[derive(Debug)]
-pub enum SheetStrokeDeserializeError {
-    InvalidFormat,
-}
-
-impl Display for SheetStrokeDeserializeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use SheetStrokeDeserializeError::*;
-        match self {
-            InvalidFormat => write!(f, "Invalid format"),
-        }
-    }
-}
-
-impl Error for SheetStrokeDeserializeError {}
-
-pub type SheetDataBytes = Vec<u8>;
-
 impl Sheet {
-    pub fn from_bytes(bytes: SheetDataBytes) -> Result<Self, SheetStrokeDeserializeError> {
+    pub fn from_bytes(bytes: Vec<u8>) -> Result<Self, IoError> {
         Self::try_from(bytes)
     }
 }
@@ -43,31 +25,31 @@ impl Sheet {
 impl SheetStroke {
     pub fn from_bytes(bytes: [u8; 4]) -> SheetStroke {
         SheetStroke {
-            x: (bytes[2] & 0x0F) << 4 + (bytes[1] & 0xF0) >> 4,
-            y: (bytes[1] & 0x0F) << 4 + (bytes[0] & 0xF0) >> 4,
+            x: (bytes[2] & 0x0F) << 4 | bytes[1].pick_bits(0..=3),
+            y: (bytes[1] & 0x0F) << 4 | bytes[0].pick_bits(0..=3),
             draw_line: bytes[2].pick_bit(6),
-            style_color: bytes[3] & 0b00000111,
+            style_color: bytes[3].pick_bits(0..=2),
             style_3d: bytes[3].pick_bit(5),
             style_bold: bytes[3].pick_bit(4),
         }
     }
 }
 
-impl TryFrom<SheetDataBytes> for Sheet {
-    type Error = SheetStrokeDeserializeError;
+impl TryFrom<Vec<u8>> for Sheet {
+    type Error = IoError;
 
-    fn try_from(value: SheetDataBytes) -> Result<Self, Self::Error> {
-        let mut reader = Box::new(Cursor::new(value));
-        let _ = reader.read_u32_le();
-        let num_blocks = reader.read_u32_le().unwrap();
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        let mut reader = Cursor::new(value);
+        reader.read_u32_le()?;
+        let num_blocks = reader.read_u32_le()?;
         reader.set_position(0x40);
 
         let mut sheet: Sheet = Sheet { strokes: vec![] };
 
         for _ in 0..num_blocks {
-            sheet.strokes.push(SheetStroke::from_bytes(
-                reader.read_const_num_of_bytes::<4>().unwrap(),
-            ));
+            sheet
+                .strokes
+                .push(SheetStroke::from_bytes(reader.read_const_num_of_bytes()?));
         }
 
         Ok(sheet)
