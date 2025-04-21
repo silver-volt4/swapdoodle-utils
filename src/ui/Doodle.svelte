21d26860ea3a11690e71eb2547cc8fbf8548936e
CenTdemeern1 <timo.herngreen@gmail.com>
2025-04-21 23:21:14 +0200

    Draw dots

--- src/ui/Doodle.svelte
+++ src/ui/Doodle.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-    import type {Sheet, Colors} from "../lib/parsing/parsing";
+    import type { Sheet, Colors } from "../lib/parsing/parsing";
 
     let canvas: HTMLCanvasElement = $state()!;
 
     interface Props {
-        sheet: Sheet
-        colors?: Colors
+        sheet: Sheet;
+        colors?: Colors;
     }
 
-    let {
-        sheet,
-        colors
-    }: Props = $props();
+    let { sheet, colors }: Props = $props();
 
     function sleep(ms: number) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+        return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
     function getColor(index: number) {
@@ -28,31 +25,38 @@
         if (!c) return;
 
         c.clearRect(0, 0, 255, 255);
+        c.lineCap = "round";
         let drawLine = false;
 
         for (let stroke of sheet.strokes) {
             if (!drawLine) {
                 c.moveTo(stroke.x, stroke.y);
                 c.beginPath();
+                c.arc(
+                    stroke.x,
+                    stroke.y,
+                    (stroke.style_bold ? 4 : 2) / 2,
+                    0,
+                    360,
+                );
+                c.fill();
+                c.beginPath();
             }
             c.lineTo(stroke.x, stroke.y);
-            c.lineWidth = stroke.style_bold ? 3 : 1;
+            c.lineWidth = stroke.style_bold ? 4 : 2;
             c.strokeStyle = getColor(stroke.style_color);
             c.stroke();
             // await sleep(16);
             drawLine = stroke.draw_line;
         }
-        c.stroke();
     }
 
     $effect(() => {
         draw();
-    })
+    });
 </script>
 
-<canvas bind:this={canvas} width="256" height="256">
-
-</canvas>
+<canvas bind:this={canvas} width="256" height="256"> </canvas>
 
 <style>
     canvas {
