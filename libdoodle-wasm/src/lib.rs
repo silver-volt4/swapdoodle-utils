2fc3d32ae0fa8cd1334576a1955e707bd1da528a
silver-volt4 <gdpancake5@gmail.com>
2025-08-14 18:43:56 +0200

    add mii viewer

--- libdoodle-wasm/src/lib.rs
+++ libdoodle-wasm/src/lib.rs
@@ -1,9 +1,7 @@
-use std::{collections::HashMap, f64::consts::E, ffi::CString, str::FromStr};
+use std::{collections::HashMap, f64::consts::E, ffi::CString, io::Read, mem, str::FromStr};
 
 use libdoodle::{
-    blocks::{colslt1::Colors, miistd1::MiiData, sheet1::Sheet},
-    bpk1::{BPK1Block, BPK1Blocks, BPK1File},
-    files::stationery::Stationery,
+    blocks::{colslt1::Colors, miistd1::{MiiData, MiiDataBytes}, sheet1::Sheet}, bpk1::{BPK1Block, BPK1Blocks, BPK1File}, error::GenericResult, files::stationery::Stationery
 };
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
@@ -28,16 +26,17 @@ pub fn decompress_if_compressed(bytes: &[u8]) -> Vec<u8> {
     libdoodle::lzss::decompress_from_slice(bytes).unwrap_or_else(|_| bytes.to_vec())
 }
 
-#[derive(Tsify, Serialize)]
-pub struct JsMii {
+#[derive(Tsify, Debug, Serialize)]
+#[tsify(into_wasm_abi)]
+pub struct MiiPreview {
     pub url: String,
     pub name: String,
     pub author_name: String,
 }
 
-impl From<MiiData> for JsMii {
+impl From<MiiData> for MiiPreview {
     fn from(value: MiiData) -> Self {
-        JsMii {
+        MiiPreview {
             url: value.get_mii_studio_url(),
             name: value.mii_name,
             author_name: value.creator_name,
@@ -84,6 +83,21 @@ pub fn parse_sheet(block: &BPK1Block) -> Result<Sheet, JsError> {
 pub fn parse_stationery(block: &BPK1Block) -> Result<Stationery, JsError> {
     match Stationery::try_from(block.data.as_slice()) {
         Ok(letter) => Ok(letter),
+        Err(e) => Err(JsError::new(format!("{:#?}", e).as_str())),
+    }
+}
+
+fn read_mii_data(block: &BPK1Block) -> GenericResult<MiiData>{
+    let mut mii_data: MiiDataBytes = [0; 0x5C];
+    let mut slice: &[u8] = &block.data;
+    slice.read(&mut mii_data)?;
+    Ok(MiiData::try_from(mii_data)?)
+}
+
+#[wasm_bindgen]
+pub fn parse_mii_data(block: &BPK1Block) -> Result<MiiPreview, JsError> {
+    match read_mii_data(block) {
+        Ok(letter) => Ok(letter.into()),
         Err(e) => Err(JsError::new(format!("{:#?}", e).as_str())),
     }
 }
---
+++ src/viewer/blocks/MIISTD1.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import BlobImage from "../../components/BlobImage.svelte";
+    import Card from "../../components/Card.svelte";
+    import { parse_mii_data } from "../../lib/libdoodle/libdoodle.svelte";
+    import type {
+        BPK1Block,
+        BPK1File,
+    } from "../../lib/libdoodle/libdoodle.svelte";
+
+    let {
+        file,
+        block,
+    }: {
+        file: BPK1File;
+        block: BPK1Block;
+    } = $props();
+
+    let miidata = $derived(parse_mii_data(block));
+</script>
+
+<Card style="info" title="About THUMB2" class="mb-2">
+    MIISTD1 blocks contain data about the sender's Mii.
+</Card>
+
+<img class="bg-green-200 rounded-full" src={miidata.url} alt="Sender's Mii"/>
+<div class="text-xs my-2">
+    <b>Note:</b> Mii images are rendered using Nintendo's Mii Studio API.<br>
+    If the image does not appear, you are either offline, or Mii Studio is down.
+</div>
+
+<div>
+    Name: <b>{miidata.name}</b>
+</div>
+{#if miidata.author_name}
+    <div>
+        Author: <b>{miidata.author_name}</b>
+    </div>
+{/if}
